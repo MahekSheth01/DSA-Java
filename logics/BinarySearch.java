d30b22841a7d15713a7c04237dfe6a86f201e849
Mahek Sheth <maheksheth77@gmail.com>
2025-07-24 18:16:16 +0530

    BinarySearch

--- logics/BinarySearch.java
+++ logics/BinarySearch.java
@@ -1,31 +1,29 @@
 package logics;
 public class BinarySearch {
-    public static int binarySearch(int[] arr, int target) {
-        int left = 0;
-        int right = arr.length - 1;
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 6, 8, 10, 12, 14}; // Sorted array
+        int key = 10;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+        int start = 0;
+        int end = arr.length - 1;
+        boolean found = false;
 
-            if (arr[mid] == target)
-                return mid; // Target found
-            else if (arr[mid] < target)
-                left = mid + 1; // Search right half
-            else
-                right = mid - 1; // Search left half
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (arr[mid] == key) {
+                System.out.println("Element found at index: " + mid);
+                found = true;
+                break;
+            } else if (arr[mid] < key) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
         }
 
-        return -1; // Target not found
-    }
-
-    public static void main(String[] args) {
-        int[] numbers = {2, 4, 6, 8, 10, 12, 14};
-        int target = 10;
-        int result = binarySearch(numbers, target);
-
-        if (result != -1)
-            System.out.println("Element found at index: " + result);
-        else
+        if (!found) {
             System.out.println("Element not found.");
+        }
     }
 }
