498c1d024dc0880db250b77f53aa80b36af2894c
Mahek Sheth <maheksheth77@gmail.com>
2025-07-25 20:10:40 +0530

    Binary to Decimal

---
+++ logics/BinaryToDecimal.java
@@ -0,0 +1,23 @@
+package logics;
+
+import java.util.Scanner;
+
+public class BinaryToDecimal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a binary number: ");
+        int binary = sc.nextInt();
+        
+        int decimal = 0;
+        int base = 1;  // 2^0
+        
+        while (binary > 0) {
+            int lastDigit = binary % 10;
+            decimal = decimal + (lastDigit * base);
+            base = base * 2;
+            binary = binary / 10;
+        }
+
+        System.out.println("Decimal value: " + decimal);
+    }
+}
