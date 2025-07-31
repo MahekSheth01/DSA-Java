079261c16a6f9c3aa7fd55a6c46c321ca62070ea
Mahek Sheth <maheksheth77@gmail.com>
2025-07-31 17:54:12 +0530

    Substring

---
+++ logics/Substring.java
@@ -0,0 +1,13 @@
+package logics;
+
+public class Substring  {
+    public static void main(String[] args) {
+        String text = "Welcome to Java";
+        if (text.contains("Java")) {
+            System.out.println("Substring found!");
+        } else {
+            System.out.println("Substring not found.");
+        }
+    }
+}
+ 
