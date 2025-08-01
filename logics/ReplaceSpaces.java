4a096c8accbe8754f72a91a6d6733560c7bafd80
Mahek Sheth <maheksheth77@gmail.com>
2025-08-01 18:24:25 +0530

    Replace Spaces

---
+++ logics/ReplaceSpaces.java
@@ -0,0 +1,9 @@
+package logics;
+
+public class ReplaceSpaces {
+    public static void main(String[] args) {
+        String input = "hello world from java";
+        String result = input.replace(" ", "-");
+        System.out.println("Output: " + result);
+    }
+}
