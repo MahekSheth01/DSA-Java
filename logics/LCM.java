65682594bfa455f1361fe3693b2b51696451c1ce
Mahek Sheth <maheksheth77@gmail.com>
2025-08-02 20:57:13 +0530

    LCM

---
+++ logics/LCM.java
@@ -0,0 +1,27 @@
+package logics;
+
+public class LCM {
+
+    // Method to find GCD using Euclidean algorithm
+    public static int findGCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    // Method to find LCM
+    public static int findLCM(int a, int b) {
+        return (a * b) / findGCD(a, b);
+    }
+
+    public static void main(String[] args) {
+        int num1 = 12, num2 = 18;
+
+        int lcm = findLCM(num1, num2);
+        System.out.println("LCM of " + num1 + " and " + num2 + " is: " + lcm);
+    }
+}
+
