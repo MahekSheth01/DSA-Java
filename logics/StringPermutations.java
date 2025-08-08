d707a4ef3abd83ed726f052ec84f61737d8d8ebe
Mahek Sheth <maheksheth77@gmail.com>
2025-08-08 18:46:40 +0530

    String Permutations

---
+++ logics/StringPermutations.java
@@ -0,0 +1,22 @@
+package logics;
+public class StringPermutations {
+
+    public static void permute(String str, String result) {
+        if (str.length() == 0) {
+            System.out.println(result);
+            return;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            String remaining = str.substring(0, i) + str.substring(i + 1);
+            permute(remaining, result + ch);
+        }
+    }
+
+    public static void main(String[] args) {
+        String input = "ABC";
+        System.out.println("All permutations of " + input + ":");
+        permute(input, "");
+    }
+}
