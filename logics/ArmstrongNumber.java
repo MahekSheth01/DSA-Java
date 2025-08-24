62efcfb8dd6173ad24119cd131750258a375193c
Mahek Sheth <maheksheth77@gmail.com>
2025-08-24 22:42:09 +0530

    Armstrong number

--- logics/ArmstrongNumber.java
+++ logics/ArmstrongNumber.java
@@ -4,15 +4,15 @@ public class ArmstrongNumber {
     public static void main(String[] args) {
         int num = 153;
         int original = num;
-        int sum = 0;
+        int sum1 = 0;
 
         while (num > 0) {
             int digit = num % 10;
-            sum += digit * digit * digit;
+            sum1 += digit * digit * digit;
             num = num / 10;
         }
 
-        if (sum == original) {
+        if (sum1 == original) {
             System.out.println(original + " is an Armstrong number");
         } else {
             System.out.println(original + " is not an Armstrong number");
