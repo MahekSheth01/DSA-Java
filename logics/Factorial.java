2e9c470cf8e9e6e32c864d01d66e315fcb97a10a
Mahek Sheth <maheksheth77@gmail.com>
2025-08-29 21:04:26 +0530

    Factorial

--- logics/Factorial.java
+++ logics/Factorial.java
@@ -3,10 +3,10 @@ package logics;
 public class Factorial {
     public static void main(String[] args) {
         int n=5;
-        int fact=1;
+        int fact1=1;
         for(int i=1;i<=n;i++){
-            fact=fact*i;
+            fact1=fact1*i;
         }
-        System.out.println(fact);
+        System.out.println(fact1);
     }
 }
