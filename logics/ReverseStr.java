80ffcc713f030b9d898eef899a32f9992c681ab0
Mahek Sheth <maheksheth77@gmail.com>
2025-08-31 20:57:41 +0530

    Reverse string

--- logics/ReverseStr.java
+++ logics/ReverseStr.java
@@ -2,7 +2,7 @@ package logics;
 
 public class ReverseStr {
     public static void main(String args[]){
-        String str="Hello World";
+        String str="MAhek sheth";
         for(int i=str.length()-1;i>=0;i--){
             System.out.print(str.charAt(i));
         }
