bdfaaec7195bebb28a541386e877c167c84ed4b3
Mahek Sheth <maheksheth77@gmail.com>
2025-09-01 19:10:24 +0530

    Substr

--- logics/Test.java
+++ logics/Test.java
@@ -2,7 +2,7 @@ package logics;
 
 public class Test {
     public static void main(String[] args) {
-        String s = "Alpha";
+        String s = "Programming";
         System.out.println(s.substring(1, 3));
     }
 }
