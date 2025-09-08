aa2af7c4bce1298aed43b2a1e67231c5731e1da6
Mahek Sheth <maheksheth77@gmail.com>
2025-09-08 19:41:17 +0530

    Bubble sort

--- sorting/BubbleSort.java
+++ sorting/BubbleSort.java
@@ -3,8 +3,6 @@ package sorting;
 public class BubbleSort {
     public static void main(String[] args) {
         int arr[]={7,8,3,4,1};
-        //Bubble Sort
-        //Largest Element --->8
         for(int i=0;i<arr.length-1;i++){
             for(int j=0;j<arr.length-i-1;j++){
                 if(arr[j]>arr[j+1]){
