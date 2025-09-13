799c4aef0762f1b4728b91c5d184abdb08c4ec52
Mahek Sheth <maheksheth77@gmail.com>
2025-09-13 22:12:41 +0530

    BubbleSort

--- sorting/BubbleSort.java
+++ sorting/BubbleSort.java
@@ -2,7 +2,7 @@ package sorting;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int arr[]={7,8,3,4,1};
+        int arr[]={10,18,13,41,12};
         for(int i=0;i<arr.length-1;i++){
             for(int j=0;j<arr.length-i-1;j++){
                 if(arr[j]>arr[j+1]){
